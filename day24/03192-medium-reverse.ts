3bde09ba1f39cdcc6135e178b8260f3a1a53ec2a
coder <liulin_suyan@163.com>
2023-06-30 09:14:06 +0800

    day24

---
+++ day24/03192-medium-reverse.ts
@@ -0,0 +1,25 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<Equal<Reverse<[]>, []>>,
+  Expect<Equal<Reverse<["a", "b"]>, ["b", "a"]>>,
+  Expect<Equal<Reverse<["a", "b", "c"]>, ["c", "b", "a"]>>
+];
+
+type errors = [
+  // @ts-expect-error
+  Reverse<"string">,
+  // @ts-expect-error
+  Reverse<{ key: "value" }>
+];
+
+// ============= Your Code Here =============
+type Reverse<T extends any[], R extends any[] = []> = T extends [
+  ...infer F,
+  infer L
+]
+  ? Reverse<F, [...R, L]>
+  : R;
+
+type T = Reverse<["a", "b"]>;
